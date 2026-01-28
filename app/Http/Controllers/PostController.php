ce09003db488a5b6621445f567d67ae573d210a1
SifanGeremu <wins33975@gmail.com>
2026-01-28 20:37:33 +0300

    feat:controller - implemented post controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+    public function index()
+    { return 'index';}
+    public function store(){
+        return 'store';
+    }
+}
